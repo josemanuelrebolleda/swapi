1a9c941b78597c2a4834c3ab828c992a400a4c8d
josemanuelrebolleda <37289399+josemanuelrebolleda@users.noreply.github.com>
2024-05-07 09:57:50 +0200

    Operativo

--- src/main/java/com/diverger/movies/controller/PersonController.java
+++ src/main/java/com/diverger/movies/controller/PersonController.java
@@ -1,9 +1,6 @@
 package com.diverger.movies.controller;
 
 import com.diverger.movies.service.PersonServiceImpl;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.ResponseEntity;
@@ -21,11 +18,6 @@ public class PersonController {
         this.personServiceImpl = personServiceImpl;
     }
 
-    @ApiOperation(value = "Get person information by name", response = ResponseEntity.class)
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Person information successfully obtained"),
-            @ApiResponse(code = 404, message = "Person with name xxxx not found")
-    })
     @GetMapping("/swapi-proxy/person-info")
     public ResponseEntity<String> getPersonInfoByName(@RequestParam String name) {
         return personServiceImpl.getPersonInfoByName(name);
